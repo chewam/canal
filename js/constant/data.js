63a7d36338fea0a7910f4ad07a840bfcc97e95c9
jerry-skeelr <jerry@skeelr.com>
2014-05-20 23:24:46 +0300

    fix image into cell

---
+++ js/constant/data.js
@@ -0,0 +1,69 @@
+app.constant('data', {
+
+	items: [{
+	    "name": "Gift 1"
+	}, {
+	    "name": "Gift 2"
+	}, {
+	    "name": "Gift 3"
+	}, {
+	    "name": "Gift 4"
+	}, {
+	    "name": "Gift 5"
+	}, {
+	    "name": "Gift 6"
+	}, {
+	    "name": "Gift 7"
+	}, {
+	    "name": "Gift 8"
+	}, {
+	    "name": "Gift 9"
+	}, {
+	    "name": "Gift 10"
+	}, {
+	    "name": "Gift 11"
+	}, {
+	    "name": "Gift 12"
+	}, {
+	    "name": "Gift 13"
+	}, {
+	    "name": "Gift 14"
+	}, {
+	    "name": "Gift 15"
+	}, {
+	    "name": "Gift 16"
+	}, {
+	    "name": "Gift 17"
+	}, {
+	    "name": "Gift 18"
+	}, {
+	    "name": "Gift 19"
+	}, {
+	    "name": "Gift 20"
+	}, {
+	    "name": "Gift 21"
+	}, {
+	    "name": "Gift 22"
+	}, {
+	    "name": "Gift 23"
+	}, {
+	    "name": "Gift 24"
+	}, {
+	    "name": "Gift 25"
+	}, {
+	    "name": "Gift 26"
+	}, {
+	    "name": "Gift 27"
+	}, {
+	    "name": "Gift 28"
+	}, {
+	    "name": "Gift 29"
+	}, {
+	    "name": "Gift 30"
+	}, {
+	    "name": "Gift 31"
+	}, {
+	    "name": "Gift 32"
+	}]
+
+});
