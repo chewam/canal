81c4e53c2870795aee79e5a32b4194d976792590
Tom <tom@skeelr.com>
2014-05-27 02:50:16 +0300

    data

--- js/constant/data.js
+++ js/constant/data.js
@@ -1,33 +1,62 @@
+/*
+BallonFoot : FF0066
+ClefUsbCube : 00CCFF
+TasseBraquo :  000000
+CoqueIphone 003399
+KitTablier 990100
+MalettePoker 006600
+CasquePliable 0000CC
+ChargeurPortable 0099CB
+ClefUsbCarte 9A33CC
+ClefUsbCube-int 000000
+CoqueIphone-int FFCC33
+EnceinteBT 0000CC
+EnceintesPortative FE0000
+KitConnexion FF6600
+Podomètre 0033FF
+RadioReveil FE0032
+SacDeSport 010066
+SetMargarita FE0032
+*/
+
 app.constant('data', {
 
     '27/05/2014': {
 
-        label: 'Jeu du jour 10',
+        label: 'Jeu du jour 9',
         img: 'img/background/day1.jpg',
         items: [{
             "name": "le coffret de tasses Braco",
-            "img":"img/items/COFFRET_TASSES_BRAQUO.jpg"
+            "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+            "colorCode" : "#000000"
         }, {
             "name": "une coque Iphone",
-            "img":"img/items/COQUE_IPHONE.jpg"
+            "img":"img/items/COQUE_IPHONE.jpg",
+            "colorCode" : "#003399"
         }, {
             "name": "le kit tablier",
-            "img":"img/items/KIT_TABLIERS.jpg"
+            "img":"img/items/KIT_TABLIERS.jpg",
+            "colorCode" : "#990100"
         }, {
             "name": "la malette Poker",
-            "img":"img/items/MALETTE_POKER.jpg"
+            "img":"img/items/MALETTE_POKER.jpg",
+            "colorCode" : "#006600"
         }, {
             "name": "le set margarita",
-            "img":"img/items/SET_MARGARITA.jpg"
+            "img":"img/items/SET_MARGARITA.jpg",
+            "colorCode" : "#FE0032"
         }, {
             "name": "le Gift 6",
-            "img":"img/items/CLE_USB_CUBE.jpg"
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
         }, {
             "name": "la cle USB",
-            "img":"img/items/CLE_USB_CUBE.jpg"
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
         }, {
             "name": "la cle USB",
-            "img":"img/items/CLE_USB_CUBE.jpg"
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
         }]
     },
 
@@ -57,6 +86,44 @@ app.constant('data', {
         }, {
             "name": "la cle USB",
             "img":"img/items/CLE_USB_CUBE.jpg"
+        }]
+    },
+    '29/05/2014': {
+
+        label: 'Jeu du jour 3',
+        img: 'img/background/day1.jpg',
+        items: [{
+            "name": "le coffret de tasses Braco",
+            "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+            "colorCode" : "#000000"
+        }, {
+            "name": "une coque Iphone",
+            "img":"img/items/COQUE_IPHONE.jpg",
+            "colorCode" : "#003399"
+        }, {
+            "name": "le kit tablier",
+            "img":"img/items/KIT_TABLIERS.jpg",
+            "colorCode" : "#990100"
+        }, {
+            "name": "la malette Poker",
+            "img":"img/items/MALETTE_POKER.jpg",
+            "colorCode" : "#006600"
+        }, {
+            "name": "le set margarita",
+            "img":"img/items/SET_MARGARITA.jpg",
+            "colorCode" : "#FE0032"
+        }, {
+            "name": "le Gift 6",
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
+        }, {
+            "name": "la cle USB",
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
+        }, {
+            "name": "la cle USB",
+            "img":"img/items/CLE_USB_CUBE.jpg",
+            "colorCode" : "#00CCFF"
         }]
     }
 
