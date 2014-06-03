8332629fc28a1168d7c24c4d8cf6b660e2c855a7
Tom <tom@skeelr.com>
2014-06-03 22:42:45 +0300

    img

--- js/constant/data.js
+++ js/constant/data.js
@@ -1,74 +1,570 @@
 app.constant('data', {
 
-    '30/05/2014': {
+   '02/06/2014': {
+       label: 'Jour 1',
+       img: 'img/background/1.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
 
-        label: 'Jeu du jour 1',
-        img: 'img/background/day1.jpg',
-        items: [{
-            "name": "BallonFoot",
-            "img":"img/items/BallonFoot.jpg",
-            "colorCode" : "#FF0066"
-        }, {
-            "name": "CasquePliable",
-            "img":"img/items/CasquePliable.jpg",
-            "colorCode" : "#0000CC"
-        }, {
-            "name": "ChargeurPortable",
-            "img":"img/items/ChargeurPortable.jpg",
-            "colorCode" : "#0099CB"
-        }, {
-            "name": "CleUsbCarte",
-            "img":"img/items/CleUsbCarte.jpg",
-            "colorCode" : "#9A33CC"
-        },{
-            "name": "CleUsbCube",
-            "img":"img/items/CleUsbCube.jpg",
-            "colorCode" : "#000000"
-        },{
-            "name": "COFFRET_TASSES_BRAQUO",
-            "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
-            "colorCode" : "#000000"
-        },  {
-            "name": "CoquesIphone",
-            "img":"img/items/CoquesIphone.jpg",
-            "colorCode" : "#FFCC33"
-        }, {
-            "name": "EnceinteBluetooth",
-            "img":"img/items/EnceinteBluetooth.jpg",
-            "colorCode" : "#0000CC"
-        }, {
-            "name": "EnceintesPortatives",
-            "img":"img/items/EnceintesPortatives.jpg",
-            "colorCode" : "#FE0000"
-        }, {
-            "name": "KIT_TABLIERS",
-            "img":"img/items/KIT_TABLIERS.jpg",
-            "colorCode" : "#990100"
-        }, {
-            "name": "KitConnexion",
-            "img":"img/items/KitConnexion.jpg",
-            "colorCode" : "#FF6600"
-        }, {
-            "name": "MALETTE_POKER",
-            "img":"img/items/MALETTE_POKER.jpg",
-            "colorCode" : "#006600"
-        }, {
-            "name": "PodometreRadioFM",
-            "img":"img/items/PodometreRadioFM.jpg",
-            "colorCode" : "#0000CC" // a faire
-        }, {
-            "name": "RadioReveil",
-            "img":"img/items/RadioReveil.jpg",
-            "colorCode" : "#FE0032"
-        }, {
-            "name": "SacSportCSAT",
-            "img":"img/items/SacSportCSAT.jpg",
-            "colorCode" : "#010066"
-        }, {
-            "name": "SET_MARGARITA",
-            "img":"img/items/SET_MARGARITA.jpg",
-            "colorCode" : "#FE0032"
-        }]
-    }
 
+
+'03/06/2014': {
+       label: 'Jour 2',
+       img: 'img/background/2.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'04/06/2014': {
+       label: 'Jour 3',
+       img: 'img/background/3.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+   '05/06/2014': {
+       label: 'Jour 4',
+       img: 'img/background/4.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },   
+
+
+
+'06/06/2014': {
+       label: 'Jour 5',
+       img: 'img/background/5.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'07/06/2014': {
+       label: 'Jour 6',
+       img: 'img/background/6.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       }]
+   },
+
+
+
+'09/06/2014': {
+       label: 'Jour 7',
+       img: 'img/background/7.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+'10/06/2014': {
+       label: 'Jour 8',
+       img: 'img/background/8.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'11/06/2014': {
+       label: 'Jour 9',
+       img: 'img/background/9.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'12/06/2014': {
+       label: 'Jour 10',
+       img: 'img/background/1.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+'13/06/2014': {
+       label: 'Jour 11',
+       img: 'img/background/2.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'14/06/2014': {
+       label: 'Jour 12',
+       img: 'img/background/3.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       }]
+   },
+
+
+
+'16/06/2014': {
+       label: 'Jour 13',
+       img: 'img/background/4.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+'17/06/2014': {
+       label: 'Jour 14',
+       img: 'img/background/5.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+'18/06/2014': {
+       label: 'Jour 15',
+       img: 'img/background/6.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+
+'19/06/2014': {
+       label: 'Jour 16',
+       img: 'img/background/7.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       }]
+   },
+
+
+
+'20/06/2014': {
+       label: 'Jour 17',
+       img: 'img/background/8.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+          "name": "COFFRET_TASSES_BRAQUO",
+           "img":"img/items/COFFRET_TASSES_BRAQUO.jpg",
+           "colorCode" : "#000000"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
+
+
+'21/06/2014': {
+       label: 'Jour 18',
+       img: 'img/background/9.jpg',
+       items: [{
+           "name": "CoquesIphone",
+           "img":"img/items/CoquesIphone.jpg",
+           "colorCode" : "#FFCC33"
+       },{
+           "name": "SET_MARGARITA",
+           "img":"img/items/SET_MARGARITA.jpg",
+           "colorCode" : "#FE0032"
+       },{
+           "name": "SacSportCSAT",
+           "img":"img/items/SacSportCSAT.jpg",
+           "colorCode" : "#010066"
+       },{
+   "name": "MALETTE_POKER",
+           "img":"img/items/MALETTE_POKER.jpg",
+           "colorCode" : "#006600"
+       },{
+           "name": "BallonFoot",
+           "img":"img/items/BallonFoot.jpg",
+           "colorCode" : "#FF0066"
+       },{
+           "name": "CleUsbCube",
+           "img":"img/items/CleUsbCube.jpg",
+           "colorCode" : "#000000"
+       }]
+   },
 });
